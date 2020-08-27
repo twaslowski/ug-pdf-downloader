b5c384075a528558c73d4ea61abe53bfc45014fe
Tobias Waslowski <tobiaswaslowski@gmail.com>
2020-08-27 11:14:03 +0200

    Adds basic config

--- src/pdf_writer.py
+++ src/pdf_writer.py
@@ -1,4 +1,10 @@
 import fpdf
+import yaml
+
+
+def read_config():
+    with open('../config.yml') as file:
+        return yaml.safe_load(file)
 
 
 def initialize_pdf():
@@ -7,30 +13,31 @@ def initialize_pdf():
     return initialized_pdf
 
 
-def configure_main_block(initialized_pdf):
-    initialized_pdf.set_font("Times", size=10)
+def _write_title(pdf, title, config):
+    pdf.set_font(config['font'], size=config['size'], style='B')
+    pdf.cell(200, 8, txt=title, ln=1, align="L")
 
 
-def configure_title(initialized_pdf):
-    initialized_pdf.set_font("Times", size=18, style='B')
+def _write_artist(pdf, artist, config):
+    pdf.set_font(config['font'], size=config['size'], style='B')
+    pdf.cell(200, 6, txt=artist, ln=1, align="L")
 
 
-def configure_artist(initialized_pdf):
-    initialized_pdf.set_font("Times", size=12, style='I')
+def _write_body(pdf, song, config):
+    pdf.set_font(config['font'], size=config['size'])
+    for line in song:
+        pdf.cell(100, 4, txt=line, ln=1, align="L")
 
 
 def write_song(title, artist, song):
+    config = read_config()
     pdf = initialize_pdf()
 
-    configure_title(pdf)
-    pdf.cell(200, 8, txt=title, ln=1, align="L")
+    _write_title(pdf, title, config['pdf']['title'])
+    _write_artist(pdf, artist, config['pdf']['artist'])
 
-    configure_artist(pdf)
-    pdf.cell(200, 6, txt=artist, ln=1, align="L")
     pdf.cell(200, 4, txt='', ln=1)
 
-    configure_main_block(pdf)
-    for line in song:
-        pdf.cell(100, 4, txt=line, ln=1, align="L")
+    _write_body(pdf, song, config['pdf']['body'])
 
     pdf.output(f"../out/{artist}-{title}.pdf")
