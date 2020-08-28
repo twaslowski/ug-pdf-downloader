168499e74682714185345058c147de41f64a72ed
Tobias Waslowski <tobiaswaslowski@gmail.com>
2020-08-28 15:29:36 +0200

    Adds requirements.txt and uses it in install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ PROJECT_ROOT=$(echo pwd)
 
 python3 -m venv .
 python3 -m pip --upgrade pip
-python3 -m pip install fpdf
+python3 -m pip install -r requirements.txt
 
 ln -s font lib/python3.8/site-packages/fpdf/font
+
+echo "All set up!"
