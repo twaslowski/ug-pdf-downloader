ee002ce1ddb3bb11b28b52448a02ca85cf4e1f5e
Tobias Waslowski <tobiaswaslowski@gmail.com>
2020-08-28 15:52:35 +0200

    Activates venv in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 PROJECT_ROOT=$(echo pwd)
 
 python3 -m venv .
-python3 -m pip --upgrade pip
+source bin/activate
 python3 -m pip install -r requirements.txt
 
 ln -s font lib/python3.8/site-packages/fpdf/font
